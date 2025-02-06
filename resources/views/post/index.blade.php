0636b2ea30d4586e81d09feb3d6f0e5a979e97fd
zirodev <zirodev23@zirodev23.com>
2025-02-06 22:22:19 +0200

    layout and post list styling added

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,11 +1,26 @@
-<h1>List of all posts</h1>
-<div>
-    <ul>
-    @foreach($posts as $post)
-        <li>
-            <h2>{{ $post->title }}</h2>
-            <p>{{ $post->content }}</p>
-        </li>
-    @endforeach
-    </ul>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('List of all posts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="post-list">
+                        <ul>
+                        @foreach($posts as $post)
+                            <li>
+                                <h2>{{ $post->title }}</h2>
+                                <p>{{ $post->content }}</p>
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
