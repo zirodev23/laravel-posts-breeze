06853856b99a8532891e8e3ee1aeb2d2d6fcd2cb
zirodev <zirodev23@zirodev23.com>
2025-02-06 23:35:52 +0200

    user can delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -68,6 +68,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -12,10 +12,18 @@
                     <div class="post-list">
                         <a href="/posts/create" type="button" class="button">Create</a>
                         <ul>
-                        @foreach($posts as $post)
+                            @foreach($posts as $post)
                             <li>
                                 <h2>{{ $post->title }}</h2>
                                 <p>{{ $post->content }}</p>
+                                <form action="/posts/{{ $post->id }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    
+                                    <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                        Delete
+                                    </button>
+                                </form>
                             </li>
                         @endforeach
                         </ul>
